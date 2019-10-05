ce287db264c88f180389d43db706199bc7ce6bb8
Fabricio Maia <fabricio.maia91@gmail,com>
2019-10-05 19:56:19 -0300

    first commit

---
+++ src/main/java/com/io/gears/api/MeuPrimeiroProjetoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.io.gears.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoSpringBootApplication.class, args);
+	}
+
+}
